e03534c468c351e552cf8a62ae4256778110401b
Jan Šilhan <silhanu@gmail.com>
2020-10-15 13:22:08 +0200

    breaking: Webpack 5. (#751)
    * new: Upgrade to webpack 5.
    
    * new: Update deps.
    
    * new: Webpack 5 fixes.
    
    * new: Add module federation.
    
    * internal: Remove deps.

--- packages/config-webpack/src/constants.ts
+++ packages/config-webpack/src/constants.ts
@@ -1,11 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 
-import postcssFlexbugsFixes from 'postcss-flexbugs-fixes';
-// @ts-expect-error
-import postcssNormalize from 'postcss-normalize';
-import postcssPresetEnv from 'postcss-preset-env';
-
 export const NUMBER_REGEX = /^(0-9)*/;
 
 export const INVALID_CHARS = /([/@\-\W])/g;
@@ -14,16 +9,16 @@ export const POSTCSS_SETTING = {
   loader: 'postcss-loader',
   options: {
     postcssOptions: {
-      ident: 'postcss',
-      plugins: () => [
-        postcssFlexbugsFixes,
-        postcssPresetEnv({
-          autoprefixer: {
-            flexbox: 'no-2009',
+      plugins: [
+        [
+          'postcss-preset-env',
+          {
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
           },
-          stage: 3,
-        }),
-        postcssNormalize(),
+        ],
       ],
     },
   },
